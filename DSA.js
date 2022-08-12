b97be7f0d96cd2416b97856802cdb29625312143
Umair <codewithumair867@gmail.com>
2022-08-13 04:45:57 +0500

    add

--- DSA.js
+++ DSA.js
@@ -51,24 +51,24 @@ Checking String Anagram - Problem 2
 
 // Solution - 1
 
-function isAnagram(string1, string2) {
-  if (string1.length !== string2.length) {
-    return false;
-  }
-  let count = {};
-  for (let letter of string1) {
-    count[letter] = (count[letter] || 0) + 1;
-    console.log(count[letter]);
-  }
-  for (let items of string2) {
-    if (!count[items]) {
-      return false;
-    }
-    count[items] -= 1;
-  }
-  return true;
-}
-const check = isAnagram("awesome", "someewa");
-console.log(check);
+// function isAnagram(string1, string2) {
+//   if (string1.length !== string2.length) {
+//     return false;
+//   }
+//   let count = {};
+//   for (let letter of string1) {
+//     count[letter] = (count[letter] || 0) + 1;
+//     console.log(count[letter]);
+//   }
+//   for (let items of string2) {
+//     if (!count[items]) {
+//       return false;
+//     }
+//     count[items] -= 1;
+//   }
+//   return true;
+// }
+// const check = isAnagram("awesome", "someewa");
+// console.log(check);
 
 // linear time complexity o(n)
