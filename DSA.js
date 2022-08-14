823f8291d081faa0f15e8e82aa42b2727b0c7308
Umair <codewithumair867@gmail.com>
2022-08-15 03:27:39 +0500

    solve

--- DSA.js
+++ DSA.js
@@ -202,3 +202,24 @@ arr1 = [1, 2, 3, 4, 5], arr2 = [1, 4, 9, 16, 25]
 // Condition
 // 1. if (arr[i] * arr[i] === arr[j])
 // 2. let isSquare = true
+
+// Solution - 1
+
+function isSquareCheck(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    let isSquare = false;
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] * arr1[i] === arr2[j]) {
+        isSquare = true;
+      }
+      if (j === arr2.length - 1) {
+        if (!isSquare) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+const result = isSquareCheck([1, 2, 3, 3, 3, 3, 2, 4, 4, 5], [1, 4, 9, 16, 25]);
+console.log("result", result);
