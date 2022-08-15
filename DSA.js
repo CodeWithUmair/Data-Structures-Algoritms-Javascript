a5b3b32f9d697dc42e2082aa483d4061284dba8d
Umair <codewithumair867@gmail.com>
2022-08-16 02:17:56 +0500

    new

--- DSA.js
+++ DSA.js
@@ -235,27 +235,29 @@ arr1 = [1, 2, 3, 4, 5], arr2 = [1, 4, 9, 16, 25]
 
 // Solution - 2 (Optimized)
 
-function checkSquare(arr1, arr2) {
-  let map1 = {};
-  let map2 = {};
-  for (item1 of arr1) {
-    map1[item1] = (map1[item1] || 0) + 1;
-  }
-  console.log("map1", map1);
-  for (item2 of arr2) {
-    map2[item2] = (map2[item2] || 0) + 1;
-  }
-  console.log("map2", map2);
-  for (let key in map1) {
-    console.log("Key", key);
-    if (!map2[key * key]) {
-      return false;
-    }
-    if (map1[key] !== map2[key * key]) {
-      return false;
-    }
-  }
-  return true;
-}
-const result = checkSquare([1, 2, 4, 2], [1, 4, 4, 16]);
-console.log("result", result);
+// function checkSquare(arr1, arr2) {
+//   let map1 = {};
+//   let map2 = {};
+//   for (item1 of arr1) {
+//     map1[item1] = (map1[item1] || 0) + 1;
+//   }
+//   console.log("map1", map1);
+//   for (item2 of arr2) {
+//     map2[item2] = (map2[item2] || 0) + 1;
+//   }
+//   console.log("map2", map2);
+//   for (let key in map1) {
+//     console.log("Key", key);
+//     if (!map2[key * key]) {
+//       return false;
+//     }
+//     if (map1[key] !== map2[key * key]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+// const result = checkSquare([1, 2, 4, 2], [1, 4, 4, 16]);
+// console.log("result", result);
+
+// timecomplexity linear O(n)
