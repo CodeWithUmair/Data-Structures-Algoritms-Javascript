a4f4ff7ea28b1b750e5d6981c2c5c4aa77025670
CodeWithUmair <codewithumair867@gmail.com>
2024-01-14 10:10:54 +0500

    Queues complete

---
+++ ZTM_DSA/Stack+Queue/queue.js
@@ -0,0 +1,52 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  peek() {
+    return this.first;
+  }
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  dequeue() {
+    if (this.length === 0) {
+      return null;
+    }
+    if (this.first === this.last) {
+      this.last = null;
+    }
+
+    this.first = this.first.next;
+    this.length--;
+    return this;
+  }
+  //isEmpty;
+}
+
+const myQueue = new Queue();
+
+console.log(myQueue.enqueue("Joy"));
+console.log(myQueue.enqueue("Matt"));
+console.log(myQueue.enqueue("Pavel"));
+console.log(myQueue.enqueue("Samir"));
+console.log(myQueue.peek());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
